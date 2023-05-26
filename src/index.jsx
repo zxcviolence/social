8505ec5ae5727015163b35f040012ffd23590279
Islam <yakubov959@mail.ru>
2023-05-26 23:15:18 +0300

    ...

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import DarkModeContextProvider from "./context/darkModeContext";
 import AuthContextProvider from "./context/authContext";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <BrowserRouter>
     <React.StrictMode>
       <DarkModeContextProvider>
