a64706d10d5db388c0441a918be50c8636b72a12
Islam <yakubov959@mail.ru>
2023-06-08 19:30:49 +0300

    ...

--- src/app/store.js
+++ src/app/store.js
@@ -2,13 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import user from "../features/userSlice";
 import theme from "../features/themeSlice";
 import data from "../features/userDataSlice";
-import like from "../features/likeSlice";
 
 export const store = configureStore({
   reducer: {
     user,
     theme,
     data,
-    like,
   },
 });
